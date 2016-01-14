32c9e730097ba38bdd2c8dc32aeb7c77a999d781
unknown <vlad.karapetyan.1988@mail.ru>
2016-01-14 15:19:55 +0400

    adding functions getImage, getIconImage and saveConfig

--- app/code/community/GluuOxd/Openid/Block/GluuOxOpenidConfig.php
+++ app/code/community/GluuOxd/Openid/Block/GluuOxOpenidConfig.php
@@ -6,5 +6,33 @@
  */
 class GluuOxd_Openid_Block_GluuOxOpenidConfig extends Mage_Core_Block_Template{
 
+    /**
+     * save config in database
+     */
+    public function saveConfig($url,$value){
+        $admin = Mage::getSingleton('admin/session')->getUser();
+        $id = $admin->getUserId();
+        $data = array($url=>$value);
+        $model = Mage::getModel('admin/user')->load($id)->addData($data);
+        try {
+            $model->setId($id)->save();
+        } catch (Exception $e){
+            Mage::log($e->getMessage(), null, 'gluuoxd_error.log', true);
+        }
+    }
+    /**
+     * geting image link
+    */
+    public function getImage($image){
+        $url = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_SKIN);
+        return $url.'adminhtml/default/default/GluuOxd_Openid/images/'.$image.'.png';
+    }
 
+    /**
+     * geting icone image link
+     */
+    public function getIconImage($image){
+        $url = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_SKIN);
+        return $url.'adminhtml/default/default/GluuOxd_Openid/images/icons/'.$image.'.png';
+    }
 }
