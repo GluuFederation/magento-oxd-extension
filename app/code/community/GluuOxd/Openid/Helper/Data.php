a8fcf831f39f5bee0922fb94b911806bd9a2befd
unknown <vlad.karapetyan.1988@mail.ru>
2016-01-15 19:11:54 +0400

    Displaying message

--- app/code/community/GluuOxd/Openid/Helper/Data.php
+++ app/code/community/GluuOxd/Openid/Helper/Data.php
@@ -6,5 +6,20 @@
  */
 class GluuOxd_Openid_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    /**
+     * displaying message
+     * @return string
+     */
+    public function displayMessage($message, $type) {
+        Mage::getSingleton ( 'core/session' )->getMessages ( true );
+        if (strcasecmp ( $type, "SUCCESS" ) == 0)
+            Mage::getSingleton ( 'core/session' )->addSuccess ( $message );
+        else if (strcasecmp ( $type, "ERROR" ) == 0)
+            Mage::getSingleton ( 'core/session' )->addError ( $message );
+        else if (strcasecmp ( $type, "NOTICE" ) == 0)
+            Mage::getSingleton ( 'core/session' )->addNotice ( $message );
+        else
+            Mage::getSingleton ( 'core/session' )->addWarning ( $message );
+    }
 
 }
