65e04f7c7be8a750c8bace5d3d38632cdc7778db
dollar007 <vlad.karapetyan.1988@mail.ru>
2016-01-19 03:39:47 -0800

    adding logout function

--- app/code/community/GluuOxd/Openid/Block/GluuOxOpenidConfig.php
+++ app/code/community/GluuOxd/Openid/Block/GluuOxOpenidConfig.php
@@ -9,7 +9,19 @@ class GluuOxd_Openid_Block_GluuOxOpenidConfig extends Mage_Core_Block_Template{
     private $getTokensByCode = "GluuOxd_Openid/getTokensByCode";
     private $getUserInfo = "GluuOxd_Openid/getUserInfo";
     private $logout = "GluuOxd_Openid/logout";
-
+    /**
+     * Administrator logout action
+     */
+    public function logout_validation()
+    {
+        $logout = $this->getLogout();
+        $config_option = unserialize(Mage::getStoreConfig ( 'gluu/oxd/oxd_config' ));
+        $oxd_id = Mage::getStoreConfig ( 'gluu/oxd/oxd_id' );
+        $logout->setRequestOxdId($oxd_id);
+        $logout->setRequestPostLogoutRedirectUri($config_option['logout_redirect_uri']);
+        $logout->setRequestIdToken($_SESSION['user_oxd_access_token']);
+        $logout->request();
+    }
     /**
      * @return string
      */
